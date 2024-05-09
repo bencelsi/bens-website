645212582060e45fe16317f014aa88cd17890372
Ben Celsi <benjamin.d.celsi@gmail.com>
2024-05-08 21:44:00 -0700

    sc

--- photo/0 scripts/listPhotos.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-for filename in os.listdir("/Users/bencelsi/LocalDrive/CODE•••••/github/bencelsi.github.io/photos"):
-    
-    capitalizedFilename = ' '.join([str.capitalize() for str in filename.split()])
-    if filename == capitalizedFilename:
-        continue
-    print("\"" + filename + "\",")
