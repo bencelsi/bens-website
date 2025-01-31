a154ba202634412087a624914252b3f7d46aaa18
Ben Celsi <benjamin.d.celsi@gmail.com>
2025-01-31 15:57:05 -0800

    sc

--- music/chords/chordify.js
+++ music/chords/chordify.js
@@ -1,17 +1,29 @@
-let text = document.getElementById("song").innerHTML.split("\n")
+const SONG = document.getElementById("song")
+
+let text = SONG.innerHTML.split("\n")
 let result = ""
 for (let i = 0; i < text.length; i++) {
     console.log(i)
     let line = text[i]
-    if (line.endsWith("``")) {
-        let clazz = "section"
-        result += "<span class='" + clazz + "'>" + line.substring(0, line.length - 2) + "</span>"
-    } else if (line.endsWith("`")) {
-        let clazz = "chords"
-        result += "<span class='" + clazz + "'>" + line.substring(0, line.length - 1) + "</span>"
-    } else {
-        result += line
-    }
+    if (line.endsWith("``")) { result += "<span class='section'>" + line.substring(0, line.length - 2) + "</span>" } 
+    else if (line.endsWith("`")) { result += "<span class='chords'>" + line.substring(0, line.length - 1) + "</span>" }
+    else { result += "<span class='lyrics'>" + line + "</span>" }
     result += "\n"
 }
-document.getElementById("song").innerHTML = result
+SONG.innerHTML = result
+
+const CHORD_BOX = document.getElementById("chordBox")
+const LYRIC_BOX = document.getElementById("lyricBox")
+const CENTER_BOX = document.getElementById("centerBox")
+const CHORDS = document.getElementsByClassName("chords")
+const LYRICS = document.getElementsByClassName("lyrics")
+
+CENTER_BOX.onclick = () => { SONG.style.textAlign = CENTER_BOX.checked ? 'center' : 'left' }
+
+CHORD_BOX.onclick = () => {
+    for (i = 0; i < CHORDS.length; i++) CHORDS[i].style.visibility = CHORD_BOX.checked ? 'visible' : 'hidden'
+}
+
+LYRIC_BOX.onclick = () => {
+    for (i = 0; i < LYRICS.length; i++) LYRICS[i].style.visibility = LYRIC_BOX.checked ? 'visible' : 'hidden'
+}
